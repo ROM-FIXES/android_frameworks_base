f6dded074949fed97829012955c736e1d60baf38
Alex <mazdarider23@gmail.com>
2016-09-07 03:45:27 -0400

    Re-add in our device utils
    In M we had too many of these. Let's establish this one early and just add checks
    to it as needed.
    
    - Add check for flashlight API
    
    Change-Id: I4a6b8ce1c365ed88b3fb14d06f4551c3d37519ec
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isWiFi only Method
    
    Change-Id: Id68bbb59811a33b57903a4a9b75c215d86252e80
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Fix isWiFi for API deprecations
    
    Change-Id: Ida8fade59dc6c10a17f93ed5384a8d30c9c4c918
    
    Utils: Add isChinese Language
    
    Change-Id: Ia9ecf8d05dcd0bfe53cdfb7a7b02651cccc9cc28
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isAvailableApp method
    
    Change-Id: Iefdaf3c0b2d5db688f3a3f2f56fbf08b535d37e4
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isPackageInstalled boolean
    
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Utils: Add isAppInstalled
    
    Change-Id: I15189741359537e0790a97e3ae832f503388ef7f
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Change-Id: I4a6b8ce1c365ed88b3fb14d06f4551c3d37519ec
    Signed-off-by: Joe Maples <joe@frap129.org>

---
+++ core/java/com/android/internal/util/evolution/EvolutionUtils.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.evolution;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+
+import java.util.Locale;
+
+public class EvolutionUtils {
+
+    public static boolean isChineseLanguage() {
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
+               Locale.CHINESE.getLanguage());
+    }
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+       Context mContext = context;
+       final PackageManager pm = mContext.getPackageManager();
+       try {
+           pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+           int enabled = pm.getApplicationEnabledSetting(packageName);
+           return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+               enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+       } catch (NameNotFoundException e) {
+           return false;
+       }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
+    }
+
+    public static boolean isWifiOnly(Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        Network[] networks = cm.getAllNetworks();
+
+        for (Network network : networks) {
+            NetworkCapabilities netCaps = cm.getNetworkCapabilities(network);
+            if (netCaps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
