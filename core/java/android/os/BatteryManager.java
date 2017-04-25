9ce12713c97d76fccbce429966d0f8d155e13127
thecrazyskull <anaskarbila@aospa.co>
2017-04-25 11:12:50 +0200

    BatteryService: add dash charging support
    Change-Id: I06e6b5a08b28a1ebda674f12e1feeb16ea029d2d
    
     Conflicts:
    	core/res/res/values/lineage_symbols.xml
    	packages/SystemUI/res/values/cm_strings.xml

--- core/java/android/os/BatteryManager.java
+++ core/java/android/os/BatteryManager.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ *           (C) 2017, The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -138,6 +139,11 @@ public class BatteryManager {
      * {@hide}
      */
     public static final String EXTRA_SEQUENCE = "seq";
+    /*
+     * boolean value to detect fast charging
+     * {@hide}
+     */
+    public static final String EXTRA_DASH_CHARGER = "dash_charger";
 
     /**
      * Extra for {@link android.content.Intent#ACTION_BATTERY_LEVEL_CHANGED}:
