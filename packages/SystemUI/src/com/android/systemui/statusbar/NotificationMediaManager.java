954861c9e435e31f54d9be6ec07ad5a09750d3c3
ezio84 <brabus84@gmail.com>
2019-11-03 21:02:11 +0100

    Ambient music: Add Pixel Now Playing support
    Show Now Playing info if no media player notification is active.
    
    Change-Id: I84c23f4bd5d1e5635aa72c68fc05b7e36a7af042

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -46,6 +46,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.provider.DeviceConfig;
 import android.provider.DeviceConfig.Properties;
+import android.text.TextUtils;
 import android.util.ArraySet;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -102,6 +103,8 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
     private static final String LOCKSCREEN_MEDIA_METADATA =
             "lineagesecure:" + LineageSettings.Secure.LOCKSCREEN_MEDIA_METADATA;
 
+    private static final String NOWPLAYING_SERVICE = "com.google.intelligence.sense";
+
     private final StatusBarStateController mStatusBarStateController
             = Dependency.get(StatusBarStateController.class);
     private final SysuiColorExtractor mColorExtractor = Dependency.get(SysuiColorExtractor.class);
@@ -144,6 +147,9 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
     private MediaController mMediaController;
     private String mMediaNotificationKey;
     private MediaMetadata mMediaMetadata;
+
+    private String mNowPlayingNotificationKey;
+    private String mNowPlayingTrack;
 
     private BackDropView mBackdrop;
     private ImageView mBackdropFront;
@@ -277,6 +283,10 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             clearCurrentMediaNotification();
             dispatchUpdateMediaMetaData(true /* changed */, true /* allowEnterAnimation */);
         }
+        if (key.equals(mNowPlayingNotificationKey)) {
+            mNowPlayingNotificationKey = null;
+            dispatchUpdateMediaMetaData(true /* changed */, true /* allowEnterAnimation */);
+        }
     }
 
     private void checkMediaNotificationColor(NotificationEntry entry) {
@@ -337,6 +347,21 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             // Promote the media notification with a controller in 'playing' state, if any.
             NotificationEntry mediaNotification = null;
             MediaController controller = null;
+
+            for (int i = 0; i < N; i++) {
+                final NotificationEntry entry = activeNotifications.get(i);
+                if (entry.notification.getPackageName().toLowerCase().equals(NOWPLAYING_SERVICE)) {
+                    mNowPlayingNotificationKey = entry.notification.getKey();
+                    final Notification n = entry.notification.getNotification();
+                    String notificationText = null;
+                    final String title = n.extras.getString(Notification.EXTRA_TITLE);
+                    if (!TextUtils.isEmpty(title)) {
+                        mNowPlayingTrack = title;
+                    }
+                    break;
+                }
+            }
+
             for (int i = 0; i < N; i++) {
                 final NotificationEntry entry = activeNotifications.get(i);
 
@@ -439,6 +464,13 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
         for (int i = 0; i < callbacks.size(); i++) {
             callbacks.get(i).onMetadataOrStateChanged(mMediaMetadata, state);
         }
+    }
+
+    public String getNowPlayingTrack() {
+        if (mNowPlayingNotificationKey == null) {
+            mNowPlayingTrack = null;
+        }
+        return mNowPlayingTrack;
     }
 
     @Override
