b69e4dcc3987724dab7e411003ec5ea6b3e5716d
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2020-06-04 19:08:06 +0000

    DataSwitchTile: Update SIMs QS icons
    * Designed and made by Andrew Fluck.
    
    Change-Id: I1b83c360029382a8a1d0598758cddf1634f7d489
    Co-authored-by: Andrew Fluck <andrew@aospa.co>
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataSwitchTile.java
@@ -233,7 +233,7 @@ public class DataSwitchTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_QUICK_TILES;
+        return MetricsEvent.CRDROID_SETTINGS;
     }
 
     /**
