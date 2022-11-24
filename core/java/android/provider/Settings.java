95d5e815da4d223b95de608cf3aea416a5e03441
LorDClockaN <lordclockan@gmail.com>
2022-11-25 00:57:41 +0530

    FWB: Sensor block per-package switch (1/2)
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    
    Fixup Sensor block
    
    Change-Id: If886a371df137cebe7482e8a8aaa888f48bebf20

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5582,6 +5582,13 @@ public final class Settings {
         public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        @Readable
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
