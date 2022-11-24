23a4a69a2c3179f605d4f55f9569645f3df34c9a
cphelps76 <cphelps76@droidconcepts.com>
2022-11-25 00:57:03 +0530

    Battery Styles: Add BatteryBar [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I2dc1859a6c9b63a96b18a1cc40eb98acf6fd5b40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -138,6 +138,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     private List<String> mBlockedIcons = new ArrayList<>();
 
+    private View mBatteryBar;
+
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
         public void setIsAirplaneMode(@NonNull IconState icon) {
@@ -232,6 +234,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mNetworkTrafficHolder = mStatusBar.findViewById(R.id.network_traffic_holder);
         mCustomIconArea = mStatusBar.findViewById(R.id.left_icon_area);
         mCenterClockLayout = mStatusBar.findViewById(R.id.centered_area);
+        mBatteryBar = mStatusBar.findViewById(R.id.battery_bar);
         mClockController = new ClockController(getContext(), mStatusBar);
         mSignalClusterEndPadding = getResources().getDimensionPixelSize(R.dimen.signal_cluster_battery_padding);
         mStatusIcons = mStatusBar.findViewById(R.id.statusIcons);
@@ -492,11 +495,13 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
 
     private void hideSystemIconArea(boolean animate) {
+        animateHide(mBatteryBar, animate);
         animateHide(mSystemIconArea, animate);
         animateHide(mNetworkTrafficHolder, animate);
     }
 
     private void showSystemIconArea(boolean animate) {
+        animateShow(mBatteryBar, animate);
         // Only show the system icon area if we are not currently animating
         int state = mAnimationScheduler.getAnimationState();
         if (state == IDLE || state == SHOWING_PERSISTENT_DOT) {
