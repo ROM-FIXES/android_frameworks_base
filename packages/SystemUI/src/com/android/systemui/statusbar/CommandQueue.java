ef808665bfb5ce340cc4a16d5aa0bdac3daf18bd
DroidFreak32 <rushabshah32@gmail.com>
2022-12-13 11:56:55 +0000

    fixup! Utils: Add api to toggle flashlight
    Change-Id: I1eb87c1b63eb04e6d6524b532b6730b4017d465d

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -1539,7 +1539,7 @@ public class CommandQueue extends IStatusBar.Stub implements
                 case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
-		    }
+		            }
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
