1c156a0849bf3b29919a86fcdf84322645ba4489
LibXZR <i@xzr.moe>
2023-04-23 14:44:23 +0530

    base: Add support for window ignore secure [1/2]
    Feature: yt/KSCOPE-23
    Change-Id: Ia9447ca17f6b24152b64526c25710a77e1388ce6
    Signed-off-by: LibXZR <i@xzr.moe>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -297,5 +297,6 @@ public class SecureSettings {
         Settings.Secure.PULSE_SOLID_UNITS_OPACITY,
         Settings.Secure.PULSE_SOLID_UNITS_ROUNDED,
         Settings.Secure.PULSE_VERTICAL_MIRROR,
+        Settings.Secure.WINDOW_IGNORE_SECURE,
     };
 }
