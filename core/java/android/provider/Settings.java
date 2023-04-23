0eb11db29e737c766ecc19e9e07a55d36889abe8
DroidFreak32 <rushabshah32@gmail.com>
2023-04-23 14:43:49 +0530

    Settings: Add custom sections
    Change-Id: Ibb587e47042cd9451439fcd21c7be22c57f99089

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5774,6 +5774,14 @@ public final class Settings {
          */
         public static final String CAMERA_FLASH_NOTIFICATION = "camera_flash_notification";
 
+        /*****************************
+        * Custom System Settings start
+        *****************************/
+
+        /***************************
+        * Custom System Settings end
+        ***************************/
+
         /**
          * Setting to enable screen flash notification feature.
          * <ul>
@@ -11545,6 +11553,14 @@ public final class Settings {
          */
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
+        /*****************************
+        * Custom Secure Settings start
+        *****************************/
+
+        /*****************************
+        * Custom Secure Settings end
+        *****************************/
+
         /**
          * Controls the 'Sunlight boost' toggle in wearable devices (high brightness mode).
          *
@@ -16889,6 +16905,15 @@ public final class Settings {
         public static final String[] TRANSIENT_SETTINGS = {
                 CLOCKWORK_HOME_READY,
         };
+
+
+        /*****************************
+        * Custom Global Settings start
+        *****************************/
+
+        /*****************************
+        * Custom Global Settings end
+        *****************************/
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
