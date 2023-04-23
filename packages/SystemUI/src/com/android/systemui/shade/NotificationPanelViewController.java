566f6efc589e0fae058b0b66904be0ac9d4f9578
xNombre <kartapolska@gmail.com>
2023-04-23 14:43:50 +0530

    Doze: Pulse Lights View only on notification
    This follows MIUI behaviour.
    
    This commit is based on: https://github.com/DerpFest-11/frameworks_base/commit/479623089c63dc539fb65c1d0717d6756af3e599
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I2eb423d7ac1dc6329be5d932b53c90e69839de26

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3250,6 +3250,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 !mDozeParameters.getDisplayNeedsBlanking() && mDozeParameters.getAlwaysOn();
         boolean pulseLights = Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
                 Settings.Secure.PULSE_AMBIENT_LIGHT, 0, UserHandle.USER_CURRENT) != 0;
+        int pulseReason = Settings.Secure.getIntForUser(mView.getContext().getContentResolver(),
+                Settings.Secure.PULSE_TRIGGER_REASON, DozeLog.PULSE_REASON_NONE, UserHandle.USER_CURRENT);
+        boolean pulseReasonNotification = pulseReason == DozeLog.PULSE_REASON_NOTIFICATION;
         if (animatePulse) {
             mAnimateNextPositionUpdate = true;
         }
@@ -3258,7 +3261,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         if (!mPulsing && !mDozing) {
             mAnimateNextPositionUpdate = false;
         }
-        if ((mPulseLightsView != null) && pulseLights) {
+        if ((mPulseLightsView != null) && pulseReasonNotification) {
             mPulseLightsView.setVisibility(mPulsing ? View.VISIBLE : View.GONE);
             if (mPulsing) {
                 mPulseLightsView.animateNotification();
