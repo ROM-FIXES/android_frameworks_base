49b7a220119226fa16c96d8891c911aa6ae17b6b
DroidFreak32 <rushabshah32@gmail.com>
2023-11-04 12:04:23 +0530

    SystemUI: Introduce DataSwitchTile
    * Based on OnePlus' OxygenOS tile, reworked to work with AOSP toggling without requirements on proprietary telephony-ext features
    
    @neobuddy89: Adapted to Android 12 and squashed below -
    
        From: DennySPB <dennyspb@gmail.com>
        Date: Thu, 5 Dec 2019 12:40:17 +0300
        Subject: DataSwitchTile: collapse notification panel onClick
    
        Change-Id: I37a064c910ea05493b41bdd0123ca3a6aca1f25d
        Signed-off-by: DennySPB <dennyspb@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 11 Mar 2020 13:36:51 +0100
        Subject: DataSwitchTile: dont show toast on click
    
        and add drawable for No SIM
    
        Change-Id: Ib9d26630879420216e67de1a1e6fc82c5c2ace5e
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: DennySPb <dennyspb@gmail.com>
        Date: Tue, 17 Nov 2020 11:39:43 +0300
        Subject: SystemUI: Show carrier name of opposite slot in DataSwitch tile label
    
        make it more user friendly
    
        Change-Id: I53094db21fe21b1a4bd9ee76fd28661622eb5e26
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Ido Ben-Hur <idoybh2@gmail.com>
        Date: Wed, 14 Jul 2021 01:43:13 +0900
        Subject: DataSwitchTile: Improve the code
    
        * Get rid of deprecated AsyncTask
        * Removed unused imports
        * Finalize global vars
        * Some other formatting and minor stuff
    
        Change-Id: Id3e39c98dac6200301d15bf752f866f47875633d
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
      @semdoc: particularly squashed with 68aae2ee3c1bb104142c5c4e6ef27d34ab8f0e3c
             SystemUI: Will you shut up, man?
    
             Silence some major logspam introduced by custom features
             Change-Id: I82a8adeef6180b33b942aea9a1fd234d291bf0e7
    
    Change-Id: Ie2e280c07f24f9da6b4ee218b72501a2713ce429
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.GestureAnywhereTile
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.CompassTile;
+import com.android.systemui.qs.tiles.DataSwitchTile;
 
 import dagger.Binds
 import dagger.Module
@@ -129,4 +130,10 @@ interface LineageModule {
     @IntoMap
     @StringKey(CompassTile.TILE_SPEC)
     fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
+
+    /** Inject DataSwitchTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(DataSwitchTile.TILE_SPEC)
+    fun bindDataSwitchTile(dataSwitchTile: DataSwitchTile): QSTileImpl<*>
 }
